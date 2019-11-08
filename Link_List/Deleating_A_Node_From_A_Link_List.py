bf9e09af03a4a74e3878e15c1e42f6c6666d617d
Om Dev Singh <devsingh640@gmail.com>
2019-11-09 03:15:07 +0530

    Add files via upload

--- Link_List/Deleating_A_Node_From_A_Link_List.py
+++ Link_List/Deleating_A_Node_From_A_Link_List.py
@@ -1,29 +1,46 @@
+import numpy as np
+
+import numpy as np
+
 class node():
-    def __init__(self, info):
-        self.node_data = info
-        self.next_node_link = None
-    def print_node(self):
-        print(node)
-class link_list():
+    def __init__(self, data):
+        self.info = data
+        self.link = None
+
+class linked_list():
     def __init__(self):
         self.head = None
-    def print_link_list(self):
+
+    def print_linked_list(self):
         temp = self.head
         while(temp):
-            print(temp.node_data, end = " ")
-            temp = temp.next_node_link
+            print(temp.info, end = " ")
+            temp = temp.link
+        
+    def delete_node(self):
+        count = 0
+        temp = self.head
+        find_value = int(input("Enter index for node which needs to be deleted : "))
+        while(temp and (count < (find_value - 1))):
+            count = (count + 1)
+            temp = temp.link
+        temp.link = temp.link.link
 
 if __name__ == "__main__":
+    x = np.random.randint(1,20)
+    my_linked_list = linked_list()
+    flag = 0
+    for el in range(0, x):
+        data = np.random.randint(0,20)
+        temp = node(data)
+        if(flag == 0):
+            my_linked_list.head = temp
+            flag = 1
+        else:
+            previous_node.link = temp
+        previous_node = temp
 
-    t = link_list()
-    t.head = node(1)
-    second_node = node(2)
-    third_node = node(3)
-    t.head.next_node_link = second_node
-    second_node.next_node_link = third_node
-    t.print_link_list()
+    my_linked_list.print_linked_list()
     print(" ")
-    deletion_node = second_node
-    t.head.next_node_link = third_node
-    second_node = None
-    t.print_link_list()
+    my_linked_list.delete_node()
+    my_linked_list.print_linked_list()
